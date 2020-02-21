bdcd982318fb0e37cca1b2a68e5c2db4aa5b3f46
Yuta Koshizawa <koher@koherent.org>
2020-02-21 14:55:29 +0900

    Implement views

--- Reversi/ViewController.swift
+++ Reversi/ViewController.swift
@@ -1,15 +1,31 @@
 import Cocoa
 
 class ViewController: NSViewController {
+    @IBOutlet private var boardView: BoardView!
     
+    @IBOutlet private var messageDiskView: DiskView!
+    @IBOutlet private var messageLabel: NSTextField!
+    @IBOutlet private var messageDiskSizeConstraint: NSLayoutConstraint!
+    private var messageDiskSize: CGFloat! // to store the size designated in the storyboard
+    
+    @IBOutlet private var darkPlayerControl: NSSegmentedControl!
+    @IBOutlet private var lightPlayerControl: NSSegmentedControl!
+    private var playerControls: [NSSegmentedControl] = []
+    
+    @IBOutlet private var darkCountLabel: NSTextField!
+    @IBOutlet private var lightCountLabel: NSTextField!
+    private var countLabels: [NSTextField] = []
+    
+    @IBOutlet private var darkPlayerActivityIndicator: NSProgressIndicator!
+    @IBOutlet private var lightPlayerActivityIndicator: NSProgressIndicator!
+    private var playerActivityIndicators: [NSProgressIndicator] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-        
-        let boardView = BoardView(frame: CGRect(x: 0, y: 0, width: 480, height: 480))
-        boardView.delegate = self
-        view.addSubview(boardView)
+        messageDiskSize = messageDiskSizeConstraint.constant
+        playerControls.append(contentsOf: [darkPlayerControl, lightPlayerControl])
+        countLabels.append(contentsOf: [darkCountLabel, lightCountLabel])
+        playerActivityIndicators.append(contentsOf: [darkPlayerActivityIndicator, lightPlayerActivityIndicator])
     }
 
     override var representedObject: Any? {
